979d43d4b5392769f7e5feb73fa2b7c230fda3bc
Marco Mendoza <marcojair38_2002@hotmail.com>
2021-01-13 20:05:47 -0600

    Docs agregado

---
+++ docs/precache-manifest.8168b5de5535c9cf418fa07880c8977e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1d3c202dfe9dd891f01cf62549042c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab467680106b5e108346",
+    "url": "/static/css/main.137a9f1c.chunk.css"
+  },
+  {
+    "revision": "a13e31d6699d5138f112",
+    "url": "/static/js/2.bed60b36.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.bed60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab467680106b5e108346",
+    "url": "/static/js/main.d5000e6a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
